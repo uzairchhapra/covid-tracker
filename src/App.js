80e871b123dcc0e83fface2e3211a410732de23a
Uzair <uzair.chhapra@deprontoinfoctech.com>
2021-03-18 21:15:08 +0530

    cases count display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetchData();
+      setData(data);
+    }
+    getData();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Cards />
+      <Cards data={data} />
       <Chart />
       <CountryPicker />
     </div>
