e4a16afdf83f24aa7cf0a80ccba9dd8e5afddc1b
Uzair <uzair.chhapra@deprontoinfoctech.com>
2021-04-16 22:37:27 +0530

    country graph

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { fetchDailyData } from '../../api';
-import { Line } from 'react-chartjs-2';
+import { Bar, Line } from 'react-chartjs-2';
 import styles from './Chart.module.css';
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country, countryData }) => {
     const [dailyData, setDailyData] = useState([]);
     useEffect(() => {
         const getDailyData = async () => {
             setDailyData(await fetchDailyData());
         }
         getDailyData();
-    });
+    }, []);
 
     const lineChart = (
         dailyData.length ? (
@@ -32,10 +32,33 @@ const Chart = () => {
                 }}
             />) : 'ERROR'
     )
+    console.log(confirmed);
+    const barChart = (
+        confirmed ? (<Bar
+            data={{
+                labels: ['Infected', 'Recovered', 'Deaths'],
+                datasets: [{
+                    data: [confirmed.value, recovered.value, deaths.value],
+                    label: 'People',
+                    backgroundColor: [
+                        'rgba(0, 0, 255, 0.5)',
+                        'rgba(0, 255, 0, 0.5)',
+                        'rgba(255, 0, 0, 0.5)'
+                    ]
+                }]
+            }}
+            options={{
+                legend: { display: false },
+                title: { display: true, text: `Current state in ${country}` },
+                // maintainAspectRatio: false,
+                // responsive: true,
+            }}
+        />) : null
+    );
 
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country && country !== 'global' ? barChart : lineChart}
         </div>
     )
 }
